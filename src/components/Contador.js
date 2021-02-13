0ddd4d634916501df131b5d37e9dc4978f61740d
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-02-12 21:39:23 -0300

    contador

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -3,37 +3,44 @@ import React from 'react'
 export default class Contador extends React.Component {
     constructor() {
         super()
-        this.state = {
-            contador: 0 
-        }
+         this.state = {
+             contador: 0
+         }
     }
 
-        decrementar() {
-            this.setState({
-                contador: this.state.contador - 1
-            })
-        }
-
-        incrementar(){
-            this.setState({
-                contador: this.state.contador + 1
-            })
-        }
-
+     aumentar() {
+         const {contador} = this.state
+         if (contador === 10) {
+             return
+         }
+         this.setState({
+             contador: contador + 1 
+         })
+     }
+     diminuir() {
+         const {contador} = this.state
+         if (contador === 0) {
+            return
+         }
+        this.setState({
+            contador: contador - 1
+        })
+     }
+      reseta(){
+          this.setState({
+              contador: 0
+          })
+      }
 
     render() {
+        const {contador} = this.state
         return(
+           
            <div>
-
-             <h1>{this.state.contador}</h1>
-
-              <div>
-
-               <button onClick={this.decrementar.bind(this)}></button>
-               <button onClick={this.incrementar.bind(this)}></button>
-
-              </div>
-
+              <h1>{contador}</h1>
+             <button onClick={this.aumentar.bind(this)}>+</button>
+             <button onClick={this.diminuir.bind(this)}>-</button>
+             <br/> <button onClick={this.reseta.bind(this)}>Resetar</button>
            </div>
         )
     }
